9ab75af27507c4b38b9622a3bf7fc1039052989a
o-strekalova <strekalova.olg@yandex.ru>
2020-05-19 20:49:21 +0300

    Добавляет фильтрация и отрисовку статистики

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
-import FilterComponent from "./components/filter.js";
-import SiteMenuComponent from "./components/site-menu.js";
+import FilterController from "./controllers/filter.js";
+import StatisticsComponent from "./components/statistics.js";
+import SiteMenuComponent, {MenuItem} from "./components/site-menu.js";
+import TasksModel from "./models/tasks.js";
 import {generateTasks} from "./mock/task.js";
-import {generateFilters} from "./mock/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const TASK_COUNT = 22;
@@ -11,14 +12,46 @@ const TASK_COUNT = 22;
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const tasks = generateTasks(TASK_COUNT);
-const filters = generateFilters();
+const siteMenuComponent = new SiteMenuComponent();
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 
-render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterComponent(filters), RenderPosition.BEFOREEND);
+const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
+const filterController = new FilterController(siteMainElement, tasksModel);
+filterController.render();
 
 const boardComponent = new BoardComponent();
-const boardController = new BoardController(boardComponent);
-
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
-boardController.render(tasks);
+const boardController = new BoardController(boardComponent, tasksModel);
+boardController.render();
+
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
+statisticsComponent.hide();
+
+siteMenuComponent.setOnChange((menuItem) => {
+  switch (menuItem) {
+    case MenuItem.NEW_TASK:
+      siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
+      boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
+      break;
+  }
+});
